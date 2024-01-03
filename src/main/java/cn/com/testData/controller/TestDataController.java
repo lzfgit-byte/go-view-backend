4b4dea57ba2c03ed0d74c637e4d6ab5f51bd005f
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-01-03 08:49:46 +0800

    i

--- src/main/java/cn/com/testData/controller/TestDataController.java
+++ src/main/java/cn/com/testData/controller/TestDataController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,13 +19,16 @@ public class TestDataController extends BaseController {
 
 
     @GetMapping("/get")
-    public AjaxResult getTestData(@RequestParam String projectId){
+    public AjaxResult getTestData(@RequestParam String projectId,HttpServletResponse response){
         Random random = new Random(System.currentTimeMillis());
+        AtomicReference<Random> autoRandom = new AtomicReference<>();
+        autoRandom.set(random);
+        String rKey = "data" + random.nextInt(5);
         List<Map<String, Object>> collect = Stream.of("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun").map(item -> {
             Map<String, Object> temp = new HashMap<>();
             temp.put("product", item);
-            temp.put("data1", random.nextInt(10));
-            temp.put("data2", random.nextInt(10));
+            temp.put("data1", autoRandom.get().nextInt(10));
+            temp.put("data2", autoRandom.get().nextInt(10));
             return temp;
         }).collect(Collectors.toList());
         System.out.println(projectId);
